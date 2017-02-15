71edd945ba10890e4d24323f289f7870d2be24ce
Mike Lambert <lambert8@illinois.edu>
2017-02-15 17:55:57 -0600

    Added Angular packages and test case

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: '<h1>Hello World!</h1>'
+})
+export class AppComponent {
+  message = 'Hello World!';
+}
--- karma-test-shim.js
+++ karma-test-shim.js
@@ -32,13 +32,38 @@ var allSpecFiles = Object.keys(window.__karma__.files)
   .filter(isBuiltFile);
 
 // (7)
-SystemJS.config({
-  baseURL: 'base'
+System.config({
+  baseURL: 'base',
+
+  map: {
+    '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+  },
 });
 
 // (8)
 System.import('systemjs.config.js')
+  .then(initTestBed)
   .then(initTesting);
+
+function initTestBed(){
+  return Promise.all([
+    System.import('@angular/core/testing'),
+    System.import('@angular/platform-browser-dynamic/testing')
+  ])
+
+  .then(function (providers) {
+    var coreTesting    = providers[0];
+    var browserTesting = providers[1];
+
+    coreTesting.TestBed.initTestEnvironment(
+      browserTesting.BrowserDynamicTestingModule,
+      browserTesting.platformBrowserDynamicTesting());
+  })
+}
 
 // (9)
 function initTesting () {
